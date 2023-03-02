92b7c34acba18f5890c27fcb8e4140f34c78de88
msorgato <mattia.sorgato@yahoo.com>
2023-03-02 14:30:13 +0100

    1st commit - Project scaffolding
    SpringBoot Initializr ftw

---
+++ src/test/java/com/maniargh/utils/backup/FileSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maniargh.utils.backup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
